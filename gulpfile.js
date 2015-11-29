385bd9f5e322f417d0237b04d5516e9ffb30d2e3
gmflanagan <gerard.flanagan@outlook.com>
2015-11-29 13:56:50 +0000

    add makefile/nodeenv harness; remove panini; add basic sticky header bar to demo page

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,14 @@ var $        = require('gulp-load-plugins')();
 var argv     = require('yargs').argv;
 var browser  = require('browser-sync');
 var gulp     = require('gulp');
-var panini   = require('panini');
 var rimraf   = require('rimraf');
 var sequence = require('run-sequence');
-var sherpa   = require('style-sherpa');
 
 // Check for --production flag
 var isProduction = !!(argv.production);
 
 // Port to use for the development server.
-var PORT = 8000;
+var PORT = 7070;
 
 // Browsers to target when prefixing CSS.
 var COMPATIBILITY = ['last 2 versions', 'ie >= 9'];
@@ -19,8 +17,10 @@ var COMPATIBILITY = ['last 2 versions', 'ie >= 9'];
 // File paths to various assets are defined here.
 var PATHS = {
   assets: [
-    'src/assets/**/*',
-    '!src/assets/{!img,js,scss}/**/*'
+    'src/**/*',
+    '!src/assets/{!img,js,scss}/**/*',
+    '!src/*~',
+    '!src/*.swp'
   ],
   sass: [
     'bower_components/foundation-sites/scss',
@@ -33,20 +33,15 @@ var PATHS = {
     'bower_components/foundation-sites/js/foundation.util.*.js',
     // Paths to individual JS components defined below
     'bower_components/foundation-sites/js/foundation.abide.js',
-    'bower_components/foundation-sites/js/foundation.accordion.js',
-    'bower_components/foundation-sites/js/foundation.accordionMenu.js',
     'bower_components/foundation-sites/js/foundation.drilldown.js',
     'bower_components/foundation-sites/js/foundation.dropdown.js',
     'bower_components/foundation-sites/js/foundation.dropdownMenu.js',
     'bower_components/foundation-sites/js/foundation.equalizer.js',
-    'bower_components/foundation-sites/js/foundation.interchange.js',
     'bower_components/foundation-sites/js/foundation.magellan.js',
-    'bower_components/foundation-sites/js/foundation.offcanvas.js',
     'bower_components/foundation-sites/js/foundation.orbit.js',
     'bower_components/foundation-sites/js/foundation.responsiveMenu.js',
     'bower_components/foundation-sites/js/foundation.responsiveToggle.js',
     'bower_components/foundation-sites/js/foundation.reveal.js',
-    'bower_components/foundation-sites/js/foundation.slider.js',
     'bower_components/foundation-sites/js/foundation.sticky.js',
     'bower_components/foundation-sites/js/foundation.tabs.js',
     'bower_components/foundation-sites/js/foundation.toggler.js',
@@ -66,33 +61,7 @@ gulp.task('clean', function(done) {
 // This task skips over the "img", "js", and "scss" folders, which are parsed separately
 gulp.task('copy', function() {
   gulp.src(PATHS.assets)
-    .pipe(gulp.dest('dist/assets'));
-});
-
-// Copy page templates into finished HTML files
-gulp.task('pages', function() {
-  gulp.src('src/pages/**/*.{html,hbs,handlebars}')
-    .pipe(panini({
-      root: 'src/pages/',
-      layouts: 'src/layouts/',
-      partials: 'src/partials/',
-      data: 'src/data/',
-      helpers: 'src/helpers/'
-    }))
     .pipe(gulp.dest('dist'));
-});
-
-gulp.task('pages:reset', function(cb) {
-  panini.refresh();
-  gulp.run('pages');
-  cb();
-});
-
-gulp.task('styleguide', function(cb) {
-  sherpa('src/styleguide/index.md', {
-    output: 'dist/styleguide.html',
-    template: 'src/styleguide/template.html'
-  }, cb);
 });
 
 // Compile Sass into CSS
@@ -132,10 +101,8 @@ gulp.task('javascript', function() {
     }));
 
   return gulp.src(PATHS.javascript)
-    .pipe($.sourcemaps.init())
     .pipe($.concat('app.js'))
     .pipe(uglify)
-    .pipe($.if(!isProduction, $.sourcemaps.write()))
     .pipe(gulp.dest('dist/assets/js'));
 });
 
@@ -153,7 +120,7 @@ gulp.task('images', function() {
 
 // Build the "dist" folder by running all of the above tasks
 gulp.task('build', function(done) {
-  sequence('clean', ['pages', 'sass', 'javascript', 'images', 'copy'], 'styleguide', done);
+  sequence('clean', ['sass', 'javascript', 'images', 'copy'], done);
 });
 
 // Start a server with LiveReload to preview the site in
@@ -166,10 +133,7 @@ gulp.task('server', ['build'], function() {
 // Build the site, run the server, and watch for file changes
 gulp.task('default', ['build', 'server'], function() {
   gulp.watch(PATHS.assets, ['copy', browser.reload]);
-  gulp.watch(['src/pages/**/*.html'], ['pages', browser.reload]);
-  gulp.watch(['src/{layouts,partials}/**/*.html'], ['pages:reset', browser.reload]);
   gulp.watch(['src/assets/scss/**/*.scss'], ['sass', browser.reload]);
   gulp.watch(['src/assets/js/**/*.js'], ['javascript', browser.reload]);
   gulp.watch(['src/assets/img/**/*'], ['images', browser.reload]);
-  gulp.watch(['src/styleguide/**'], ['styleguide', browser.reload]);
 });
---
+++ makefile
@@ -0,0 +1,48 @@
+
+SHELL := /bin/bash
+VIRTUAL_ENV ?= $(abspath .pyenv)
+VENV := $(VIRTUAL_ENV)
+PYTHON := python3
+PIP := $(VENV)/bin/pip
+NODE := $(VENV)/bin/node
+NODE_VERSION := 4.2.2
+NPM := $(VENV)/bin/npm
+BOWER := $(abspath node_modules/.bin/bower)
+GULP := $(abspath node_modules/.bin/gulp)
+DIST_ROOT := $(abspath dist)
+DEBUG ?= 0
+
+export VIRTUAL_ENV
+
+.PHONY: venv develop serve publish clean nuke
+
+venv:
+	@if [ ! -e $(VENV) ]; then virtualenv -v -p $(PYTHON) $(VENV); fi
+	@$(PIP) install -U pip
+	@$(PIP) install -r requirements.txt
+
+nodejs:
+	@if [ ! -e $(NODE) ]; then \
+		echo "Installing nodejs. This may take a few minutes." && $(VENV)/bin/nodeenv -v -p --node=$(NODE_VERSION); \
+	fi
+
+install: venv nodejs
+	@source $(VENV)/bin/activate && $(NPM) install && $(BOWER) install
+
+start:
+	@source $(VENV)/bin/activate && $(NPM) start
+
+build:
+	@source $(VENV)/bin/activate && $(GULP) build --production
+
+publish: build
+	@s3cmd sync $(DIST_ROOT)/ s3://$(S3_BUCKET) --acl-public --delete-removed --guess-mime-type
+
+clean:
+	@rm -rf $(DIST_ROOT)
+
+nuke: clean
+	@rm -rf $(VIRTUAL_ENV)
+	@rm -rf ./node_modules
+	@rm -rf ./bower_components
+
